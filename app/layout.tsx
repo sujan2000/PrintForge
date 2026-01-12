3a7f996f003180ef5d44bcf25764f71ebd747ec0
sujan2000 <meena.sujan06@gmail.com>
2026-01-12 22:52:32 +0530

    about page add

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,11 @@
 import "./globals.css";
+import { Albert_Sans } from "next/font/google"
 
+
+const albertSans = Albert_Sans({
+  subsets: ["latin"],
+  display: "swap"
+})
 
 export default function RootLayout({
   children,
@@ -8,7 +14,29 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className={albertSans.className}>
+        <header className="w-full bg-white">
+          <nav className="flex justify-between px-6 py-4">
+            <div className="relative">
+              {/* Desktop logo */}
+              <img
+                src="/printforge-logo-desktop.svg"
+                alt="PrintForge Logo"
+                className="w-50 h-auto hidden md:block"
+              />
+              {/* Mobile logo */}
+              <img
+                src="/printforge-logo-mobile.svg"
+                alt="PrintForge Logo"
+                className="w-10 h-auto block md:hidden"
+              />
+            </div>
+            <ul className="flex items-center gap-2.5">
+              <p>3D Models</p>
+              <p>About</p>
+            </ul>
+          </nav>
+        </header>
         {children}
       </body>
     </html>
